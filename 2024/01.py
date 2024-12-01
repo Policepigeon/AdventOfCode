89f910b3d64632aa05b3049154b8ac5d6393f1be
Policepigeon <policepigeon01@gmail.com>
2024-12-01 13:26:18 +0000

    this still doesnt work. let's try an excel spreadsheet

--- 2024/01.py
+++ 2024/01.py
@@ -1,13 +1,22 @@
-#the first day of coding
-list1 = [3,4,2,1,3,3]
-list2 = [4,3,5,3,9,3]
-totaldiff = 0
-list1 = sorted(list1)
-list2 = sorted(list2)
-print(list1,list2)
-for i in range(1,len(list1)):
-  diff = abs(list1[i]-list2[i])
-  totaldiff = totaldiff + diff 
-  print("diff = ",diff)
-  print("tdiff =",totaldiff)
-print(totaldiff)
+def calculate_distance(left_list, right_list):
+    # Sort both lists before calculating the distance
+    sorted_left = sorted(left_list)
+    sorted_right = sorted(right_list)
+
+    # Calculate the difference between corresponding elements in both lists
+    distances = [abs(sorted_left[i] - sorted_right[i]) for i in range(min(len(sorted_left), len(sorted_right)))]
+
+
+    return sum(distances)
+
+def main():
+    with open('/home/policepigeon/Documents/code/AdventOfCode/2024/2024⁄01.txt', 'r') as f:
+        puzzle_input = [int(x) for x in f.read().split()]
+
+    left_list, right_list = puzzle_input[:len(puzzle_input)//2], puzzle_input[len(puzzle_input)//2:]
+    
+    distance = calculate_distance(left_list, right_list)
+    print(distance)
+
+if __name__ == "__main__":
+    main()
